36611f7464d64830821885d60527c78c02d91241
Valikwp <valikpbua@gmail.com>
2025-03-03 22:04:53 +0200

    added file type, fixed, removed test data

--- frontend/src/FileManager/FileManager.jsx
+++ frontend/src/FileManager/FileManager.jsx
@@ -15,66 +15,39 @@ import PropTypes from "prop-types";
 import { dateStringValidator, urlValidator } from "../validators/propValidators";
 import "./FileManager.scss";
 
-const testFiles = [
-  {
-    name: "Documents",
-    isDirectory: true, // Folder
-    path: "/Documents", // Located in Root directory
-    updatedAt: "2024-09-09T10:30:00Z", // Last updated time
-  },
-  {
-    name: "Pictures",
-    isDirectory: true,
-    path: "/Pictures", // Located in Root directory as well
-    updatedAt: "2024-09-09T11:00:00Z",
-  },
-  {
-    name: "Pic.png",
-    isDirectory: false, // File
-    path: "/Pictures/Pic.png", // Located inside the "Pictures" folder
-    updatedAt: "2024-09-08T16:45:00Z",
-    size: 2048, // File size in bytes (example: 2 KB)
-    type: "png",
-  },
-];
-
 const FileManager = ({
-                       fileUploadConfig = {
-                         url: "/upload",
-                         headers: {},
-                         method: "POST",
-                       },
-                       isLoading = false,
-                       onCreateFolder = () => {},
-                       onFileUploading = () => {},
-                       onFileUploaded = () => {},
-                       onCut = () => {},
-                       onCopy = () => {},
-                       onPaste = () => {},
-                       onRename = () => {},
-                       onDownload = () => {},
-                       onDelete = () => {},
-                       onLayoutChange = () => {},
-                       onRefresh = () => {},
-                       onFileOpen = () => {},
-                       onSelect = () => {},
-                       onError = () => {},
-                       layout = "grid",
-                       enableFilePreview = true,
-                       maxFileSize = 1000000,
-                       filePreviewPath = "",
-                       acceptedFileTypes = "",
-                       height = "600px",
-                       width = "100%",
-                       initialPath = "",
-                       filePreviewComponent = null,
-                       primaryColor = "#6155b4",
-                       fontFamily = "Nunito Sans, sans-serif",
-                     }) => {
+  files,
+  fileUploadConfig,
+  isLoading,
+  onCreateFolder,
+  onFileUploading = () => {},
+  onFileUploaded = () => {},
+  onCut,
+  onCopy,
+  onPaste,
+  onRename,
+  onDownload,
+  onDelete = () => null,
+  onLayoutChange = () => {},
+  onRefresh,
+  onFileOpen = () => {},
+  onSelect,
+  onError = () => {},
+  layout = "grid",
+  enableFilePreview = true,
+  maxFileSize,
+  filePreviewPath,
+  acceptedFileTypes,
+  height = "600px",
+  width = "100%",
+  initialPath = "",
+  filePreviewComponent,
+  primaryColor = "#6155b4",
+  fontFamily = "Nunito Sans, sans-serif",
+}) => {
   const triggerAction = useTriggerAction();
   const { containerRef, colSizes, isDragging, handleMouseMove, handleMouseUp, handleMouseDown } =
-      useColumnResize(20, 80);
-
+    useColumnResize(20, 80);
   const customStyles = {
     "--file-manager-font-family": fontFamily,
     "--file-manager-primary-color": primaryColor,
@@ -83,72 +56,80 @@ const FileManager = ({
   };
 
   return (
-      <main className="file-explorer" onContextMenu={(e) => e.preventDefault()} style={customStyles}>
-        <Loader loading={isLoading} />
-        <FilesProvider filesData={testFiles} onError={onError}>
-          <FileNavigationProvider initialPath={initialPath}>
-            <SelectionProvider onDownload={onDownload} onSelect={onSelect}>
-              <ClipBoardProvider onPaste={onPaste} onCut={onCut} onCopy={onCopy}>
-                <LayoutProvider layout={layout}>
-                  <Toolbar
-                      allowCreateFolder
-                      allowUploadFile
-                      onLayoutChange={onLayoutChange}
-                      onRefresh={onRefresh}
-                      triggerAction={triggerAction}
-                  />
-                  <section
-                      ref={containerRef}
-                      onMouseMove={handleMouseMove}
-                      onMouseUp={handleMouseUp}
-                      className="files-container"
-                  >
-                    <div className="navigation-pane" style={{ width: colSizes.col1 + "%" }}>
-                      <NavigationPane onFileOpen={onFileOpen} />
-                      <div
-                          className={`sidebar-resize ${isDragging ? "sidebar-dragging" : ""}`}
-                          onMouseDown={handleMouseDown}
-                      />
-                    </div>
+    <main className="file-explorer" onContextMenu={(e) => e.preventDefault()} style={customStyles}>
+      <Loader loading={isLoading} />
+      <FilesProvider filesData={files} onError={onError}>
+        <FileNavigationProvider initialPath={initialPath}>
+          <SelectionProvider onDownload={onDownload} onSelect={onSelect}>
+            <ClipBoardProvider onPaste={onPaste} onCut={onCut} onCopy={onCopy}>
+              <LayoutProvider layout={layout}>
+                <Toolbar
+                  allowCreateFolder
+                  allowUploadFile
+                  onLayoutChange={onLayoutChange}
+                  onRefresh={onRefresh}
+                  triggerAction={triggerAction}
+                />
+                <section
+                  ref={containerRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseUp={handleMouseUp}
+                  className="files-container"
+                >
+                  <div className="navigation-pane" style={{ width: colSizes.col1 + "%" }}>
+                    <NavigationPane onFileOpen={onFileOpen} />
+                    <div
+                      className={`sidebar-resize ${isDragging ? "sidebar-dragging" : ""}`}
+                      onMouseDown={handleMouseDown}
+                    />
+                  </div>
 
-                    <div className="folders-preview" style={{ width: colSizes.col2 + "%" }}>
-                      <BreadCrumb />
-                      <FileList
-                          onCreateFolder={onCreateFolder}
-                          onRename={onRename}
-                          onFileOpen={onFileOpen}
-                          onRefresh={onRefresh}
-                          enableFilePreview={enableFilePreview}
-                          triggerAction={triggerAction}
-                      />
-                    </div>
-                  </section>
-
-                  <Actions
-                      fileUploadConfig={fileUploadConfig}
-                      onFileUploading={onFileUploading}
-                      onFileUploaded={onFileUploaded}
-                      onDelete={onDelete}
+                  <div className="folders-preview" style={{ width: colSizes.col2 + "%" }}>
+                    <BreadCrumb />
+                    <FileList
+                      onCreateFolder={onCreateFolder}
+                      onRename={onRename}
+                      onFileOpen={onFileOpen}
                       onRefresh={onRefresh}
-                      maxFileSize={maxFileSize}
-                      filePreviewPath={filePreviewPath}
-                      filePreviewComponent={filePreviewComponent}
-                      acceptedFileTypes={acceptedFileTypes}
+                      enableFilePreview={enableFilePreview}
                       triggerAction={triggerAction}
-                      files={testFiles}
-                  />
-                </LayoutProvider>
-              </ClipBoardProvider>
-            </SelectionProvider>
-          </FileNavigationProvider>
-        </FilesProvider>
-      </main>
+                    />
+                  </div>
+                </section>
+
+                <Actions
+                  fileUploadConfig={fileUploadConfig}
+                  onFileUploading={onFileUploading}
+                  onFileUploaded={onFileUploaded}
+                  onDelete={onDelete}
+                  onRefresh={onRefresh}
+                  maxFileSize={maxFileSize}
+                  filePreviewPath={filePreviewPath}
+                  filePreviewComponent={filePreviewComponent}
+                  acceptedFileTypes={acceptedFileTypes}
+                  triggerAction={triggerAction}
+                />
+              </LayoutProvider>
+            </ClipBoardProvider>
+          </SelectionProvider>
+        </FileNavigationProvider>
+      </FilesProvider>
+    </main>
   );
 };
 
 FileManager.displayName = "FileManager";
 
 FileManager.propTypes = {
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      isDirectory: PropTypes.bool.isRequired,
+      path: PropTypes.string.isRequired,
+      updatedAt: dateStringValidator,
+      size: PropTypes.number,
+    })
+  ).isRequired,
   fileUploadConfig: PropTypes.shape({
     url: urlValidator,
     headers: PropTypes.objectOf(PropTypes.string),
